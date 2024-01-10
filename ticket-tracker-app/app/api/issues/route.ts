6981e088cac925a30e699c93fa0a81b7de69beb8
Arnoldo Marin <arnoldo.emarin@gmail.com>
2024-01-10 15:49:05 +0100

    Implement error handling and server side validation

--- ticket-tracker-app/app/api/issues/route.ts
+++ ticket-tracker-app/app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
+    title: z.string().min(1, 'Title is required.').max(255),
+    description: z.string().min(1, 'Description is required.'),
 })
 // API for creating issues
 export async function POST(request: NextRequest) {
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
     const validation = createIssueSchema.safeParse(body);
     if (!validation.success) {
         // invalid values provided by client
-        return NextResponse.json(validation.error.errors, { status: 400})
+        return NextResponse.json(validation.error.format(), { status: 400})
     }
 
     const newIssue = await prisma.issue.create({
